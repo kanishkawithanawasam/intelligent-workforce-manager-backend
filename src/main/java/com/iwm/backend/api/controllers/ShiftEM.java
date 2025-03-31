60c33e3cff7186bb89782048288c8cdbe305b6d5
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-03-31 20:46:36 +0100

    Addition: ShiftEM implemented

---
+++ src/main/java/com/iwm/backend/api/controllers/ShiftEM.java
@@ -0,0 +1,36 @@
+package com.iwm.backend.api.controllers;
+
+import com.iwm.backend.schedulegenerator.models.WeeklySchedule;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "shift")
+public class ShiftEM {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "shift_date")
+    private LocalDate date;
+
+    @Column(name = "start_time")
+    private LocalTime startTime;
+
+    @Column(name = "end_time")
+    private LocalTime endTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_shift_employee"))
+    private EmployeeEM employee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_shift_schedule"))
+    private WeeklySchedule weeklySchedule;
+}
