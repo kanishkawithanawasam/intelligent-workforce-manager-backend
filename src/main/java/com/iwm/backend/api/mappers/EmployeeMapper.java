e5588379e6c2a3e38d7d932bc81ac4fb0ecc3af0
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-04-03 00:18:59 +0100

    Addition: EmployeeMapper

---
+++ src/main/java/com/iwm/backend/api/mappers/EmployeeMapper.java
@@ -0,0 +1,38 @@
+package com.iwm.backend.api.mappers;
+
+import com.iwm.backend.api.models.ContractDataEM;
+import com.iwm.backend.api.models.EmployeeEM;
+import com.iwm.backend.api.models.EmployeePreferencesEM;
+import com.iwm.backend.schedulegenerator.models.Employee;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class EmployeeMapper {
+
+    public Employee toDomain(EmployeeEM em){
+
+        if(em.getContractData().isEmpty()){
+            return new Employee();
+        }else{
+            int lastContractEntry = em.getContractData().size()-1;
+            int lastPreferenceEntry = em.getPreferences().size()-1;
+
+            ContractDataEM contractData =  em.getContractData().get(lastContractEntry);
+            EmployeePreferencesEM preferences = em.getPreferences().get(lastPreferenceEntry);
+            return new Employee(
+                    em.getId(),
+                    em.getFirstName()+ " "+ em.getFirstName(),
+                    contractData.getRole(),
+                    preferences.getPreferredHours(),
+                    contractData.getMaxHoursPerWeek(),
+                    contractData.getHorlyRate()
+            );
+        }
+    }
+
+    public List<Employee> toDomainList(List<EmployeeEM> employeeEMList){
+        return employeeEMList.stream().map(this::toDomain).toList();
+    }
+}
