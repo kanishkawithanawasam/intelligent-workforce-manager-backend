1010182c2c8c0c28a15ea509ba6afe87845f13b4
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-04-02 12:18:41 +0100

    Refactor: Demand Preset added

--- src/main/java/com/iwm/backend/schedulegenerator/exceptions/DemandNotFoundException.java
+++ src/main/java/com/iwm/backend/schedulegenerator/exceptions/DemandNotFoundException.java
@@ -3,6 +3,6 @@ package com.iwm.backend.schedulegenerator.exceptions;
 public class DemandNotFoundException extends RuntimeException {
 
     public DemandNotFoundException() {
-        super("Demand Not Found! Please check your parameters.");
+        super("DemandPreset Not Found! Please check your parameters.");
     }
 }
--- src/main/java/com/iwm/backend/schedulegenerator/exceptions/InvalidDemandMapException.java
+++ src/main/java/com/iwm/backend/schedulegenerator/exceptions/InvalidDemandMapException.java
@@ -2,6 +2,6 @@ package com.iwm.backend.schedulegenerator.exceptions;
 
 public class InvalidDemandMapException extends RuntimeException {
     public InvalidDemandMapException() {
-        super("Demand map is inconsistent! Please check if the values in demand is valid");
+        super("DemandPreset map is inconsistent! Please check if the values in demand is valid");
     }
 }
