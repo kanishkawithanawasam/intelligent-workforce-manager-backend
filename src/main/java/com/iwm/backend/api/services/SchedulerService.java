7ba2908c52e79ba4ecf1d66df754ad5d845d5c07
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-04-11 19:37:47 +0100

    Fix: minor fixes

--- src/main/java/com/iwm/backend/api/services/SchedulerService.java
+++ src/main/java/com/iwm/backend/api/services/SchedulerService.java
@@ -1,5 +1,7 @@
 package com.iwm.backend.api.services;
 
+import com.iwm.backend.api.controllers.exceptions.ScheduleNotFoundException;
+import com.iwm.backend.api.dtos.ScheduleRequestDTO;
 import com.iwm.backend.api.dtos.WeeklyScheduleDTO;
 import com.iwm.backend.api.dtos.mappers.EmployeeDomainMapper;
 import com.iwm.backend.api.dtos.mappers.WeeklyScheduleDTOMapper;
@@ -8,6 +10,7 @@ import com.iwm.backend.api.repository.EmployeeRepository;
 import com.iwm.backend.api.repository.WeeklyScheduleRepository;
 import com.iwm.backend.schedulegenerator.FuzzyGeneticScheduleGenerator;
 import com.iwm.backend.schedulegenerator.models.Employee;
+import com.iwm.backend.schedulegenerator.models.WeeklySchedule;
 import com.iwm.backend.trial.DemandReader;
 import org.springframework.stereotype.Service;
 
@@ -29,44 +32,59 @@ public class SchedulerService {
     }
 
 
-    public WeeklyScheduleDTO getWeekScheduleFromWeek(LocalDate date) throws IOException {
-        return WeeklyScheduleDTOMapper.toWeeklyScheduleDTO(weeklyScheduleRepository.findByScheduleStartDate(date));
+    public WeeklyScheduleDTO getWeekScheduleFromDate(LocalDate startDate){
+        WeeklyScheduleDTO weeklyScheduleDTO = WeeklyScheduleDTOMapper.toWeeklyScheduleDTO(
+                weeklyScheduleRepository.findByScheduleStartDate(startDate));
+
+        // Return an empty schedule DTO is the weekly schedule is null.
+        if (weeklyScheduleDTO == null) {
+            weeklyScheduleDTO = new WeeklyScheduleDTO(LocalDate.now(),new ArrayList<>(),0.0);
+        }
+
+        return weeklyScheduleDTO;
     }
 
     public List<LocalDate> getWeekStartDates() {
 
         List<LocalDate> weekStartDates = new ArrayList<>();
 
+
         // Determine the last schedule date
         LocalDate latestWeekStartDate = weeklyScheduleRepository.findLatestWeekStartDate();
+        if (latestWeekStartDate == null) {
+            latestWeekStartDate = LocalDate.now();
+        }
+
 
         LocalDate dateCounter = LocalDate.now();
-        if(latestWeekStartDate == null || latestWeekStartDate.isBefore(LocalDate.now())) {
+        List<LocalDate> unavailableStartDates = weeklyScheduleRepository.findStartDatesBetween(dateCounter, latestWeekStartDate);
+        for (int i = 0; i <8; i++) {
             dateCounter = dateCounter.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
-            weekStartDates.add(dateCounter);
-        }else{
-            List<LocalDate> unavailableStartDates = weeklyScheduleRepository.findStartDatesBetween(dateCounter, latestWeekStartDate);
-            for (int i = 0; i <8; i++) {
-                dateCounter = dateCounter.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
-                if(!unavailableStartDates.contains(dateCounter)) {
-                    weekStartDates.add(dateCounter);
-                }
+            if(!unavailableStartDates.contains(dateCounter)) {
+                weekStartDates.add(dateCounter);
             }
         }
+
         return weekStartDates;
     }
 
-    public WeeklyScheduleDTO generateWeeklySchedule(LocalDate startDate) throws IOException {
+    public WeeklyScheduleDTO generateWeeklySchedule(ScheduleRequestDTO requestDTO) throws IOException {
         List<Employee> employees = EmployeeDomainMapper.toDomainList(employeeRepository.findAll());
-        FuzzyGeneticScheduleGenerator generator =
+        WeeklySchedule schedule =
                 new FuzzyGeneticScheduleGenerator(employees,
-                        DemandReader.getDemand());
-        WeeklyScheduleDTO dto = WeeklyScheduleDTOMapper.toWeeklyScheduleDTO(generator.genSchedule());
-        dto.setScheduleStartDate(startDate);
+                        DemandReader.getDemand()).genSchedule();
+
+        if (schedule == null) {
+            throw new ScheduleNotFoundException();
+        }
+        WeeklyScheduleDTO dto = WeeklyScheduleDTOMapper.toWeeklyScheduleDTO(schedule);
+        assert dto != null;
+        dto.setScheduleStartDate(requestDTO.getStartDate());
         return dto;
     }
 
-    public void saveWeeklySchedule(WeeklyScheduleDTO weeklyScheduleDTO) throws IOException {
+
+    public void saveWeeklySchedule(WeeklyScheduleDTO weeklyScheduleDTO){
         WeeklyScheduleEM saved = weeklyScheduleRepository.save(
                 WeeklyScheduleDTOMapper.toWeeklyScheduleEM(weeklyScheduleDTO));
     }
