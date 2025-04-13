1dbf8d46cda933357148970a6068d57277aa0272
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-04-13 13:22:35 +0100

    Addition: ContractDataService - ContractDataService() added

---
+++ src/main/java/com/iwm/backend/api/services/ContractDataService.java
@@ -0,0 +1,26 @@
+package com.iwm.backend.api.services;
+
+
+import com.iwm.backend.api.dtos.contractdata.ContractDataDTO;
+import com.iwm.backend.api.dtos.mappers.ContractDataMapper;
+import com.iwm.backend.api.models.ContractDataEM;
+import com.iwm.backend.api.repository.ContractRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContractDataService {
+
+    private final ContractRepository contractRepository;
+
+    public ContractDataService(ContractRepository contractRepository) {
+        this.contractRepository = contractRepository;
+    }
+
+    @Transactional
+    public ContractDataDTO getLatestContractData(long employeeId) {
+        ContractDataEM contractDataEM=contractRepository.findLatestContractDataByEmployeeId(employeeId);
+        return ContractDataMapper.toContractDataDTO(contractDataEM);
+    }
+
+}
