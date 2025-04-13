258f278ba5ce1c6dd04b6348284e3dd3e66c9dc4
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-04-13 13:19:00 +0100

    Fix: ContractDataDTO - typo fix

--- src/main/java/com/iwm/backend/api/dtos/contractdata/ContractDataDTO.java
+++ src/main/java/com/iwm/backend/api/dtos/contractdata/ContractDataDTO.java
@@ -11,6 +11,5 @@ public class ContractDataDTO {
     private double maxHoursPerWeek;
     private LocalDate startDate;
     private LocalDate endDate;
-    private String contractType;
-
+    private String role;
 }
