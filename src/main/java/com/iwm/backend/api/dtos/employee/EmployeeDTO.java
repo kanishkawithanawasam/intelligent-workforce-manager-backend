9136eb21eb1d0488857c3963d5bd62644219900c
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-04-13 03:09:09 +0100

    Addition: EmployeeDTO removed

---
+++ src/main/java/com/iwm/backend/api/dtos/employee/EmployeeDTO.java
@@ -0,0 +1,17 @@
+package com.iwm.backend.api.dtos.employee;
+
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class EmployeeDTO {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String address;
+    private String contact;
+    private String postalCode;
+}
