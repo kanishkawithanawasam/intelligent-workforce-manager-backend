e66788da06c380fd4d60b8a5377ed43c7ae79668
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-04-13 03:10:05 +0100

    Refactor: EmployeeDTO added, URL refactored to RESTful complient

--- src/main/java/com/iwm/backend/api/controllers/EmployeeController.java
+++ src/main/java/com/iwm/backend/api/controllers/EmployeeController.java
@@ -1,11 +1,9 @@
 package com.iwm.backend.api.controllers;
 
-import com.iwm.backend.api.dtos.EmployeeBasicInfoDTO;
-import com.iwm.backend.api.dtos.EmployeeFileDTO;
-import com.iwm.backend.api.models.EmployeeEM;
-import com.iwm.backend.api.repository.EmployeeRepository;
+import com.iwm.backend.api.dtos.employee.EmployeeBasicInfoDTO;
+import com.iwm.backend.api.dtos.employee.EmployeeDTO;
 import com.iwm.backend.api.services.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,15 +18,15 @@ public class EmployeeController{
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/employees-basic-info")
+    @GetMapping("/basic-info")
     public List<EmployeeBasicInfoDTO> getEmployeeBasicInfo() {
         return employeeService.getEmployeesBasicInfo();
     };
 
-    @GetMapping("/employee-file/{id}")
-    public EmployeeFileDTO getEmployeeFile(@PathVariable long id) {
-        System.out.println("id:" + id);
-        return employeeService.getEmployeeFile(id);
+    @GetMapping("/demographics/{id}")
+    public ResponseEntity<EmployeeDTO> getEmployeeFile(@PathVariable long id) {
+        EmployeeDTO dto = employeeService.getEmployee(id);
+        return ResponseEntity.ok().body(dto);
     }
 
 }
