d5bb926d9ba3ba50ae70e30c722f1813d8936b0a
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-04-16 22:15:36 +0100

    Addition: UserEM added

---
+++ src/main/java/com/iwm/backend/api/models/UserEM.java
@@ -0,0 +1,27 @@
+package com.iwm.backend.api.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Users")
+@Data
+public class UserEM {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long id;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "Access_Roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "access_role_id")
+    )
+    private Set<AccessRoleEM> roles = new HashSet<>();
+}
