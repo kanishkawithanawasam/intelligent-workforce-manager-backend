19b1f2891f3738576c579c6b708b60c97260895d
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-08-14 10:09:22 +0100

    Refactor: Unused import removed - EmployeeMapperTest

--- rest-api/src/test/java/com/iwm/backend/modules/schedules/WeeklyScheduleDTOMapperTest.java
+++ rest-api/src/test/java/com/iwm/backend/modules/schedules/WeeklyScheduleDTOMapperTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
